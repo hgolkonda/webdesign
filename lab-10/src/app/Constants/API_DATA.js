4fdc8ee63d08f1ae638afa5dc3b6b642220ebbd1
hgolkonda <145042834+hgolkonda@users.noreply.github.com>
2023-11-26 22:10:01 -0500

    Add files via upload

---
+++ lab-10/src/app/Constants/API_DATA.js
@@ -0,0 +1,4 @@
+export const GEO_URL = 'http://api.openweathermap.org/geo/1.0/'
+export const API_KEY = '62e32e38f7d20ecfc394fcefa1ee3df8';
+
+export const WEATHER_URL = 'http://api.openweathermap.org/data/2.5';
